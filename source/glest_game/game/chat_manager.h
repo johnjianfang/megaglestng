f085ccb6d198dffd7b6bef9f7bd5f2280191e6ca
titi_ger <titi_ger@03bcafef-9c4d-45c2-aa05-caeccb650a8a>
2010-04-15 21:16:13 +0000

    press escape to abort chatting
    git-svn-id: https://megaglest.svn.sourceforge.net/svnroot/megaglest/trunk@253 03bcafef-9c4d-45c2-aa05-caeccb650a8a

--- source/glest_game/game/chat_manager.h
+++ source/glest_game/game/chat_manager.h
@@ -1,7 +1,7 @@
 // ==============================================================
 //	This file is part of Glest (www.glest.org)
 //
-//	Copyright (C) 2001-2008 Martiño Figueroa
+//	Copyright (C) 2001-2008 Martiï¿½o Figueroa
 //
 //	You can redistribute this code and/or modify it under 
 //	the terms of the GNU General Public License as published 
@@ -40,6 +40,7 @@ public:
 	void init(Console* console, int thisTeamIndex);
 
 	void keyDown(char key);
+	void keyUp(char key);
 	void keyPress(char c);
 	void updateNetwork();
 

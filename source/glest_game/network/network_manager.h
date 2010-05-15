3c939ecb0913a3612fbb203ec918e0f077ee9425
mvejvoda <mvejvoda@03bcafef-9c4d-45c2-aa05-caeccb650a8a>
2010-05-15 18:59:17 +0000

    - Added partial threaded network server handling of incoming socket data to try to improve performance - Small changes to client network handling to be more accurate and efficient - Added observer mode when a user loses a network game
    git-svn-id: https://megaglest.svn.sourceforge.net/svnroot/megaglest/trunk@372 03bcafef-9c4d-45c2-aa05-caeccb650a8a

--- source/glest_game/network/network_manager.h
+++ source/glest_game/network/network_manager.h
@@ -1,7 +1,7 @@
 // ==============================================================
 //	This file is part of Glest (www.glest.org)
 //
-//	Copyright (C) 2001-2008 Martiño Figueroa
+//	Copyright (C) 2001-2008 Martio Figueroa
 //
 //	You can redistribute this code and/or modify it under 
 //	the terms of the GNU General Public License as published 
@@ -50,6 +50,7 @@ public:
 	GameNetworkInterface* getGameNetworkInterface();
 	ServerInterface* getServerInterface();
 	ClientInterface* getClientInterface();
+	NetworkRole getNetworkRole() { return networkRole; }
 };
 
 }}//end namespace

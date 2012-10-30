1ff3a5316578aa51498971c449aa36931a046ab3
mvejvoda <mvejvoda@03bcafef-9c4d-45c2-aa05-caeccb650a8a>
2012-10-30 06:48:12 +0000

    - attempt to cleanup for debian compliance (proper copyright, etc)
    git-svn-id: https://megaglest.svn.sourceforge.net/svnroot/megaglest/trunk@3790 03bcafef-9c4d-45c2-aa05-caeccb650a8a

--- source/shared_lib/include/graphics/d3d9/graphics_factory_d3d9.h
+++ source/shared_lib/include/graphics/d3d9/graphics_factory_d3d9.h
@@ -1,3 +1,14 @@
+// ==============================================================
+//	This file is part of Glest (www.glest.org)
+//
+//	Copyright (C) 2001-2008 Martiño Figueroa
+//
+//	You can redistribute this code and/or modify it under
+//	the terms of the GNU General Public License as published
+//	by the Free Software Foundation; either version 2 of the
+//	License, or (at your option) any later version
+// ==============================================================
+
 #ifndef _SHARED_D3D9_GRAPHICSFACTORYD3D9_H_
 #define _SHARED_D3D9_GRAPHICSFACTORYD3D9_H_
 
